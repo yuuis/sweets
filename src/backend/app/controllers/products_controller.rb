a8d2cac70a3a394246aa354ba8ae0df3087a8b29
Shota Harikae <shota@sorahane.me>
2018-12-09 07:00:30 +0900

    add products create show

--- src/backend/app/controllers/products_controller.rb
+++ src/backend/app/controllers/products_controller.rb
@@ -1,9 +1,29 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     products = Product.active_all
     render json: ActiveModel::Serializer::CollectionSerializer.new(
         products,
         each_serializer: ProductSerializer
     ).to_json
+  end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product, status: :ok
+  end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product, status: :created
+    else
+      render json: { errors: product.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price)
   end
 end
--- src/backend/config/routes.rb
+++ src/backend/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
-      resources :products, only: [:index]
+      resources :products, only: [:index, :create, :show]
     end
   end
 end
