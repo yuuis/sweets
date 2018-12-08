62a49c0c9738397a93b47901d86d002ec8800b29
Shota Harikae <shota@sorahane.me>
2018-12-09 02:13:05 +0900

    fix puroduct index

--- src/backend/app/controllers/products_controller.rb
+++ src/backend/app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: ActiveModel::ArraySerializer.new(
+    products = Product.active_all
+    render json: ActiveModel::Serializer::CollectionSerializer.new(
         products,
         each_serializer: ProductSerializer
     ).to_json
--- src/backend/app/models/product.rb
+++ src/backend/app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   has_many :purchased_product, dependent: :restrict_with_error
+  scope :active_all, -> { where(deleted: false) }
 end
