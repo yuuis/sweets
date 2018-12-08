dee52e877ba0620ee85bfd7fa01f44980654ae2f
Shota Harikae <shota@sorahane.me>
2018-12-08 21:44:15 +0900

    init rails

--- src/backend/README.md
+++ src/backend/README.md
@@ -1,4 +1,24 @@
-# setting up
-```bash
-  $ bundle install --path vendor/bundle
-```
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+Things you may want to cover:
+
+* Ruby version
+
+* System dependencies
+
+* Configuration
+
+* Database creation
+
+* Database initialization
+
+* How to run the test suite
+
+* Services (job queues, cache servers, search engines, etc.)
+
+* Deployment instructions
+
+* ...
--- src/backend/config/application.rb
+++ src/backend/config/application.rb
@@ -9,15 +9,15 @@ require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
-# require "action_cable/engine"
-require "sprockets/railtie"
+require "action_cable/engine"
+# require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Sweets
+module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -26,5 +26,10 @@ module Sweets
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
   end
 end
