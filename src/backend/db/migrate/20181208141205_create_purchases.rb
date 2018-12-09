55a87ca6bcc8e9223f12b8e3ae706ab497be806a
Yu Ishiguro <yuuis.egr@gmail.com>
2018-12-09 09:57:29 +0900

    change column

--- src/backend/db/migrate/20181208141205_create_purchases.rb
+++ src/backend/db/migrate/20181208141205_create_purchases.rb
@@ -2,7 +2,7 @@ class CreatePurchases < ActiveRecord::Migration[5.2]
   def change
     create_table :purchases do |t|
       t.integer :user_id, null: false
-      t.boolean :paid, null: false, :default => false
+      t.string :status, null: false, :default => "waiting" # success / waiting / failed
       t.timestamps
     end
   end
