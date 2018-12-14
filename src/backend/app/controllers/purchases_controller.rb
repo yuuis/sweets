4a4f65b2766de16dcb1122ddb4f1515dc517e363
Yu Ishiguro <yuuis.egr@gmail.com>
2018-12-15 03:14:56 +0900

    fix make id wont be null

--- src/backend/app/controllers/purchases_controller.rb
+++ src/backend/app/controllers/purchases_controller.rb
@@ -3,19 +3,19 @@ class PurchasesController < ApplicationController
 
   def reserve
     purchase = Purchase.new(status: :waiting)
-    purchase.save
-    purchased_product = params["_json"].map do |param|
-      PurchasedProduct.new(product_id: param[:product_id], quantity: param[:quantity], purchase_id: purchase.id)
+    params["_json"].map do |param|
+      purchase.purchased_product.new(product_id: param[:product_id], quantity: param[:quantity])
     end
 
-    if (PurchasedProduct.import purchased_product).failed_instances.empty?
-      render json: purchased_product, status: :created
+    if purchase.save
+      render json: purchase.purchased_product, status: :created
       # felica readerをactiveにする処理
       # purchase_idも一緒に送る
     else
       render json: { errors: purchase.errors }, status: :unprocessable_entity
     end
   end
+
 
   def create
     purchase = Purchase.find(params[:purchase_id])
