7d0362033fc701ef43e53219ea3fe5ea0e42e9f8
Shota Harikae <shota@sorahane.me>
2018-12-21 04:35:33 +0900

    link idm reader

--- src/backend/app/controllers/purchases_controller.rb
+++ src/backend/app/controllers/purchases_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+require 'uri'
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
@@ -8,26 +10,35 @@ class PurchasesController < ApplicationController
     end
 
     if purchase.save
+      fetch_api('http://' + ENV.fetch('IDM_READER_HOST_NAME') + '/api/v1/purchase', purchase.to_json,)
       render json: purchase, status: :created
-      # felica readerをactiveにする処理
-      # purchase_idも一緒に送る
     else
       render json: { errors: purchase.errors }, status: :unprocessable_entity
     end
   end
 
-
   def create
-    purchase = Purchase.find(params[:purchase_id])
-    user = User.find(Idm.find_by(value: params[:idm]).user_id)
+    print params
+    purchase = Purchase.find(params[:body][:purchase_id])
+    user = User.find(Idm.find_by(value: params[:body][:idm]).user_id)
     if purchase && user
       purchase.user_id = user.id
+      purchase.status = :success
       if purchase.save
         render json: purchase, status: :created
       else
-        render json: {errors: purchase.errors}, status: :unprocessable_entity
+        render json: { errors: purchase.errors }, status: :unprocessable_entity
       end
     end
   end
 
+  def fetch_api(url, data)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req['Content-Type'] = 'application/json'
+    req.body = data
+    res = http.request(req)
+    # TODO レスポンスエラー処理
+  end
 end
