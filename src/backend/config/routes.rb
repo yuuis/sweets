69f5083820846fe368beec3cd93f5aff7dbd50a7
sho <>
2019-01-16 01:41:22 +0900

    fixed namespace

--- src/backend/config/routes.rb
+++ src/backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  scope :api do
+  namespace :api do
     scope :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :products, only: [:index, :create, :show]
